f0011d5687a242e49e2c68aecdd808b312ff475b
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-09-09 01:37:01 +0530

    Grid Commit

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
 
 const Grid = () => {
   return (
     <section id='about'>
-        
+        <BentoGrid>
+            {[{ title: 'Title1', description: 'Desc1', id: 1}].map((item) => (
+                <BentoGridItem
+                    id={item.id}
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}
+                />
+            ))}
+        </BentoGrid>
     </section>
   )
 }
