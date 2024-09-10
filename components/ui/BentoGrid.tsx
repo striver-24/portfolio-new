2305a2d8f766104fcda83494a1177b2fb98ebed6
Deivyansh Singh <106349960+striver-24@users.noreply.github.com>
2024-09-11 00:30:21 +0530

    Final Commit BentoGrid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -49,7 +49,7 @@ export const BentoGridItem = ({
       )}
       style={{
         background: 'rgb(2,0,36)',
-        BackgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(93,108,111,1) 26%, rgba(0,212,255,1) 100%)',
+        backgroundColor: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(93,108,111,1) 26%, rgba(0,212,255,1) 100%)',
       }}
     >
       <div className={`${id === 6} && 'flex justify-center h-full'`}>
@@ -62,6 +62,18 @@ export const BentoGridItem = ({
             />
           )}
         </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+            {spareImg && (
+              <img
+                src={spareImg}
+                alt={spareImg}
+                className={('object-cover, object-center')}
+              />
+            )}
+        </div>
+        {id === 6 && (
+          
+        )}
       </div>
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
